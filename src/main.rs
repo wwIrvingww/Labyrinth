eb1b8e32a3a022c4a4b31305d40d931b4d6edddd
wwIrvingww <mor22781@uvg.edu.gt>
2024-07-22 22:18:45 -0600

    reader text file added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod maze {
+    pub mod reader;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "maze.txt";
+    let maze = maze::reader::load_maze(filename);
+
+    // Imprimir el laberinto cargado
+    for row in maze {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
 }
